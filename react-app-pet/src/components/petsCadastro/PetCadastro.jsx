6301e766980c2135e0cb692ba9243039fe7c617a
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2022-10-02 20:19:34 -0300

    FAZENDO TRATATIVAS DE ERRO!

--- react-app-pet/src/components/petsCadastro/PetCadastro.jsx
+++ react-app-pet/src/components/petsCadastro/PetCadastro.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+const DisplayingErrorMessagesSchema = Yup.object().shape({
+    nome: Yup.string()
+        .required('Campo obrigatório!'),
+    idade: Yup.number()
+        .max(21, "Idade acima do permitido")
+        .required('Campo obrigatório!'),
+    raca: Yup.string()
+        .required('Campo obrigatório!'),
+    tamanho: Yup.string()
+        .required('Campo obrigatório!'),
+    nomeDono: Yup.string()
+        .required('Campo obrigatório!'),
+    telDono: Yup.string()
+        .required("Telefone é obrigatório")
+});
+
 const PetCadastro = () => {
     return(
         <div>
